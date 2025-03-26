25f1d02fbd0259bb038a838d680d4030a898d565
Ahrorbek Orziqulov <mlbbahror@gmail.com>
2025-03-26 20:07:44 +0500

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  
-    path('taomlar/<int:restorant_id>/',views.TaomView, name='TaomView'),
-    path('savat/', views.savat, name='savat'), 
-]
